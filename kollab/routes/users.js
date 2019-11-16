dabd0647b52cef98c38a94bbd06ff8926f0cc43e
adriana-abella <56332223+adriana-abella@users.noreply.github.com>
2019-11-16 13:02:28 -0500

    Add files via upload

--- kollab/routes/users.js
+++ kollab/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
+
+//bring in User Model
+
+let User = require('../models/user');
+
+//Register Form
+
+router.get('/register', function(req, res, next){
+  res.render('register');
+
+});
+
 
 module.exports = router;
