401d277933af0dc942b218ea5cd12315c0acd5e1
Konrad Gapinski <36116903+CoderKonrad@users.noreply.github.com>
2019-11-24 17:30:21 -0500

    Add files via upload

--- kollab/app.js
+++ kollab/app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mainRouter = require('./routes/mainPage')
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mainPage', mainRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,4 +48,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
